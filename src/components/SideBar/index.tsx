43134620def12a9a6c1d64e29fec5750b0f9b843
rogeraf15 <roger.afernandes@hotmail.com>
2020-07-10 18:58:53 -0300

    SideBar's scroll, just like twitter

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StickyBox from 'react-sticky-box';
 
 import List from '../List';
 import FollowSuggestion from '../FollowSuggestion';
@@ -14,40 +15,42 @@ const SideBar: React.FC = () => {
                 <SearchIcon/>
             </SearchWrapper>
 
-            <Body>
-                <List
-                    title="Talvez você curta"
-                    elements={[
-                        <FollowSuggestion name="Siclano" nickname="@siclanotest"/>,
-                        <FollowSuggestion name="Beltrano" nickname="@beltranotest"/>,
-                        <FollowSuggestion name="Fulano" nickname="@fulanotest"/>
-                    ]}
-                />
-                <List
-                    title="Oque está acontecendo"
-                    elements={[
-                        <News/>,
-                        <News/>,
-                        <News/>,
-                    ]}
-                />
-                 <List
-                    title="Oque está acontecendo"
-                    elements={[
-                        <News/>,
-                        <News/>,
-                        <News/>,
-                    ]}
-                />
-                 <List
-                    title="Oque está acontecendo"
-                    elements={[
-                        <News/>,
-                        <News/>,
-                        <News/>,
-                    ]}
-                />
-            </Body>
+            <StickyBox>
+                <Body>
+                    <List
+                        title="Talvez você curta"
+                        elements={[
+                            <FollowSuggestion name="Siclano" nickname="@siclanotest"/>,
+                            <FollowSuggestion name="Beltrano" nickname="@beltranotest"/>,
+                            <FollowSuggestion name="Fulano" nickname="@fulanotest"/>
+                        ]}
+                    />
+                    <List
+                        title="Oque está acontecendo"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>,
+                        ]}
+                    />
+                    <List
+                        title="Oque está acontecendo"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>,
+                        ]}
+                    />
+                    <List
+                        title="Oque está acontecendo"
+                        elements={[
+                            <News/>,
+                            <News/>,
+                            <News/>,
+                        ]}
+                    />
+                </Body>
+            </StickyBox>
         </Container>
     );
 }
--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -70,6 +70,9 @@ export const Body = styled.div`
     padding: 57px 24px 200px;
     margin-top: 3px;
 
+    position: sticky;
+    top: 1;
+
     >div + div {
         margin-top: 15px;
     }
